97f34f0fb4a2abd3db15b8d9d4f7007e9cf04e64
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 18:38:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(press) {
+    if (press.which === 71) { // G = 71
+      alert('G was pressed.');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+  alert('You entered the wrong value.');
+  return;
+}
 
 $(document).ready(function(){
 
