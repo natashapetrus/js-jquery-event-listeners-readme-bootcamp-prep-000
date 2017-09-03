2da6a96c6ec0cb09eca169e3695c455f4fb4167d
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 18:37:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,16 +13,19 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on("keydown", function(press) {
-    if (press.which === 71) {
-      alert("You have entered g.");
+    if (press.which === 71) { // G = 71
+      alert('G was pressed.');
     }
-  })
+  });
 }
 
 function submitIt() {
-  $('form').on("submit", function() {
-    alert("Your form is going to be submitted now.");
-  })
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+  alert('You entered the wrong value.');
+  return;
 }
 
 $(document).ready(function(){
